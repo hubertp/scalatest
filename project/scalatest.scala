64bd055822eef7c0935f511dd84d1c4074159f1a
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-08-15 13:54:28 +0000

    sbtbuild branch - Tweaked to be able to build with 3GB heap size.

--- project/scalatest.scala
+++ project/scalatest.scala
@@ -24,29 +24,47 @@ object ScalatestBuild extends Build {
                               "org.scalatest.events.TestLocationMethodTestNGSuite", 
                               "org.scalatest.events.TestLocationTestNGSuite", 
                               "org.scalatest.tools.SomeApiClassRunner", 
-                              "org.scalatest.PackageAccessConstructorSuite")
+                              "org.scalatest.PackageAccessConstructorSuite", 
+                              "org.scalatest.TestColonEscapeExampleTestNGSuite", 
+                              "org.scalatest.TestColonEscapeExamplePathFunSpec", 
+                              "org.scalatest.ExampleClassTaggingJUnit3Suite", 
+                              "org.scalatest.TestColonEscapeExampleJUnit3Suite", 
+                              "org.scalatest.TestColonEscapeExamplePathFreeSpec", 
+                              "org.scalatest.TestColonEscapeExampleJUnitSuite", 
+                              "org.scalatest.NotAccessibleSuite")
                               
-   lazy val root = Project("scalatest", file(".")) settings(
+   lazy val scalatest = Project("scalatest", file("."))
+   .settings(
      organization := "org.scalatest",
-     version := "2.0-2.9.0-M3",
+     version := "2.0-M3",
      scalaVersion := "2.9.0",
      libraryDependencies ++= simpledependencies,
      resolvers += "Sonatype Public" at "https://oss.sonatype.org/content/groups/public",
      genCodeTask, 
      sourceGenerators in Compile <+= 
-         (baseDirectory, sourceManaged in Compile) map genFiles("gen")(GenGen.genMain),
-     sourceGenerators in Test <+= 
-         (baseDirectory, sourceManaged in Test) map genFiles("gen")(GenGen.genTest),
+         (baseDirectory, sourceManaged in Compile) map genFiles("gen", "GenGen.scala")(GenGen.genMain),
      sourceGenerators in Compile <+= 
-         (baseDirectory, sourceManaged in Compile) map genFiles("gentables")(GenTable.genMain),
-     sourceGenerators in Test <+= 
-         (baseDirectory, sourceManaged in Test) map genFiles("gentables")(GenTable.genTest), 
+         (baseDirectory, sourceManaged in Compile) map genFiles("gentables", "GenTable.scala")(GenTable.genMain),
      sourceGenerators in Compile <+=
-         (baseDirectory, sourceManaged in Compile) map genFiles("mustmachers")(GenMustMatchers.genMain), 
-     sourceGenerators in Test <+= 
-         (baseDirectory, sourceManaged in Test) map genFiles("mustmatchers")(GenMustMatchers.genTest), 
+         (baseDirectory, sourceManaged in Compile) map genFiles("mustmatchers", "GenMustMatchers.scala")(GenMustMatchers.genMain),  
      testOptions in Test := Seq(Tests.Filter(className => isIncludedTest(className)))
-   )
+   ).aggregate("gentests")
+   
+   lazy val gentests = Project("gentests", file("gen"))
+   .settings(
+     organization := "org.scalatest",
+     version := "2.0-M3",
+     scalaVersion := "2.9.0",
+     libraryDependencies ++= simpledependencies,
+     resolvers += "Sonatype Public" at "https://oss.sonatype.org/content/groups/public",
+     sourceGenerators in Test <+= 
+         (baseDirectory, sourceManaged in Test) map genFiles("gen", "GenGen.scala")(GenGen.genTest),
+     sourceGenerators in Test <+= 
+         (baseDirectory, sourceManaged in Test) map genFiles("gentables", "GenTable.scala")(GenTable.genTest), 
+     sourceGenerators in Test <+= 
+         (baseDirectory, sourceManaged in Test) map genFiles("mustmatchers", "GenMustMatchers.scala")(GenMustMatchers.genTest),
+     testOptions in Test := Seq(Tests.Filter(className => isIncludedTest(className)))
+   ).dependsOn(scalatest  % "test->test")
 
    def isIncludedTest(className: String) = {
      try {
@@ -62,7 +80,7 @@ object ScalatestBuild extends Build {
 
    def simpledependencies = Seq(
      "org.scala-tools.testing" % "test-interface" % "0.5",  // TODO optional
-     "org.scalacheck" %% "scalacheck" % "1.8",   // TODO optional
+     "org.scalacheck" %% "scalacheck" % "1.10.0",   // TODO optional
      "org.easymock" % "easymockclassextension" % "3.1",   // TODO optional
      "org.jmock" % "jmock-legacy" % "2.5.1", // TODO optional
      "org.mockito" % "mockito-all" % "1.9.0", // TODO optional
@@ -78,11 +96,11 @@ object ScalatestBuild extends Build {
      "org.eclipse.jetty" % "jetty-webapp" % "8.0.1.v20110908" % "test"
   )
   
-  def genFiles(name: String)(gen: File => Unit)(basedir: File, outDir: File): Seq[File] = {
+  def genFiles(name: String, generatorSource: String)(gen: File => Unit)(basedir: File, outDir: File): Seq[File] = {
     val tdir = outDir / name
-    val genTableSource = basedir / "project" / "GenTable.scala"
+    val genSource = basedir / "project" / generatorSource
     def results = (tdir ** "*.scala").get
-    if (results.isEmpty || results.exists(_.lastModified < genTableSource.lastModified)) {
+    if (results.isEmpty || results.exists(_.lastModified < genSource.lastModified)) {
       tdir.mkdirs()
       gen(tdir)
     }
