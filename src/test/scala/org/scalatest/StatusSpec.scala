9ba69109db55dc561d2387cf203d8c20470112c1
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-09-25 07:34:38 +0000

    runstatus2 branch - Rewritten StatusSpec using withFixture and duck typing to reduce code duplication.

--- src/test/scala/org/scalatest/StatusSpec.scala
+++ src/test/scala/org/scalatest/StatusSpec.scala
@@ -1,66 +1,44 @@
 package org.scalatest
 
-class StatusSpec extends Spec {
-
-  object `ScalaTestStatefulStatus` {
-    def `should by default return false for isCompleted` {
-      val status = new ScalaTestStatefulStatus
-      assert(!status.isCompleted)
-    }
-    
-    def `should return true for isCompleted after completes() is called` {
-      val status = new ScalaTestStatefulStatus
-      status.completes()
-      assert(status.isCompleted)
-    }
-    
-    def `should return true for succeeds() after completes() is called without fails()` {
-      val status = new ScalaTestStatefulStatus
-      status.completes()
-      assert(status.succeeds)
-    }
-    
-    def `should return false for succeeds() after completes is called after fails()` {
-      val status = new ScalaTestStatefulStatus
-      status.fails()
-      status.completes()
-      assert(!status.succeeds)
-    }
-    
-    def `waitUntilCompleted should not block after completes() is called` {
-      val status = new ScalaTestStatefulStatus
-      status.completes()
-      status.waitUntilCompleted()
-    }
+class StatusSpec extends fixture.Spec {
+  
+  protected type FixtureParam = { 
+    def completes()
+    def isCompleted: Boolean
+    def succeeds(): Boolean
+    def fails()
+    def waitUntilCompleted()
   }
   
-  object `StatefulStatus` {
-    def `should by default return false for isCompleted` {
-      val status = new StatefulStatus
+   override protected def withFixture(test: OneArgTest) {
+     val status1 = new ScalaTestStatefulStatus
+     test(status1)
+     val status2 = new StatefulStatus
+     test(status2)
+   }
+  
+  object `StatefulStatus ` {
+    def `should by default return false for isCompleted`(status: FixtureParam) {
       assert(!status.isCompleted)
     }
     
-    def `should return true for isCompleted after completes() is called` {
-      val status = new StatefulStatus
+    def `should return true for isCompleted after completes() is called`(status: FixtureParam) {
       status.completes()
       assert(status.isCompleted)
     }
     
-    def `should return true for succeeds() after completes() is called without fails()` {
-      val status = new StatefulStatus
+    def `should return true for succeeds() after completes() is called without fails()`(status: FixtureParam) {
       status.completes()
       assert(status.succeeds)
     }
     
-    def `should return false for succeeds() after completes is called after fails()` {
-      val status = new StatefulStatus
+    def `should return false for succeeds() after completes is called after fails()`(status: FixtureParam) {
       status.fails()
       status.completes()
       assert(!status.succeeds)
     }
     
-    def `waitUntilCompleted should not block after completes() is called` {
-      val status = new StatefulStatus
+    def `waitUntilCompleted should not block after completes() is called`(status: FixtureParam) {
       status.completes()
       status.waitUntilCompleted()
     }
