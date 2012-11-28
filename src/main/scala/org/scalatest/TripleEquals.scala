7799374eb4b8a647621d0470851cd8f6e547e223
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-11-28 20:07:34 +0000

    Init ver

---
+++ src/main/scala/org/scalatest/TripleEquals.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest
+
+trait TripleEquals extends AnyEquality {
+
+  class AnyEqualizer[L](left: L) {
+    def ===[R](right: R)(implicit equality: Equality[L], constraint: EqualityConstraint[L, R]): Boolean = equality.areEqual(left, right)
+    def !==[R](right: R)(implicit equality: Equality[L], constraint: EqualityConstraint[L, R]): Boolean = !equality.areEqual(left, right)
+  }
+
+  implicit def convertToAnyEqualizer[T](o: T) = new AnyEqualizer(o)
+}
+
+object TripleEquals extends TripleEquals
+
